a183b4321b5fb1938dac8e0b85e1941f58ee64da
Dustuurn1 <96318134+Dustuurn1@users.noreply.github.com>
2025-04-10 22:15:58 -0400

    Update routes.dart

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import '../views/user_profile_view.dart';
 import '../views/make_coffee_view.dart';
 import '../views/main_menu_view.dart';
+import '../views/stats_view.dart';
+import '../views/signin_view.dart';
 // Import other views
 
 class Routes {
-  static const String mainMenu = '/';
+  static const String login = '/';
+  static const String mainMenu = '/main-menu';
   static const String userProfile = '/user-profile';
   static const String makeCoffee = '/make-coffee';
   static const String statistics = '/stat-coffee';
@@ -13,10 +16,11 @@ class Routes {
 
   static Map<String, WidgetBuilder> getRoutes() {
     return {
+      login: (context) => SignInView(),
       mainMenu: (context) => MainMenuView(),
       userProfile: (context) => UserProfileView(),
       makeCoffee: (context) => MakeCoffeeView(),// Add other routes
-      //statistics: (context) => StatisticsView(),
+      statistics: (context) => StatisticsView(),
     };
   }
 }
